156cc67daf48834179a52400ed08e29c187577f7
Naiguel Santos <naiguelsantos@Naiguels-MacBook-Pro.local>
2019-05-28 15:34:14 -0300

    update manifest json icons

--- docs/lideco-app/build/main.js
+++ docs/lideco-app/build/main.js
@@ -858,19 +858,19 @@ var map = {
 		4
 	],
 	"../pages/products/products.module": [
-		495,
+		496,
 		3
 	],
 	"../pages/reset-password/reset-password.module": [
-		496,
+		495,
 		2
 	],
 	"../pages/signup/signup.module": [
-		497,
+		498,
 		1
 	],
 	"../pages/splash/splash.module": [
-		498,
+		497,
 		0
 	]
 };
@@ -1183,10 +1183,10 @@ var AppModule = /** @class */ (function () {
                         { loadChildren: '../pages/list/list.module#ListPageModule', name: 'ListPage', segment: 'list', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/login/login.module#LoginPageModule', name: 'LoginPage', segment: 'login', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/product-create/product-create.module#ProductCreatePageModule', name: 'ProductCreatePage', segment: 'product-create', priority: 'low', defaultHistory: [] },
-                        { loadChildren: '../pages/products/products.module#ProductsPageModule', name: 'ProductsPage', segment: 'products', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/reset-password/reset-password.module#ResetPasswordPageModule', name: 'ResetPasswordPage', segment: 'reset-password', priority: 'low', defaultHistory: [] },
-                        { loadChildren: '../pages/signup/signup.module#SignupPageModule', name: 'SignupPage', segment: 'signup', priority: 'low', defaultHistory: [] },
-                        { loadChildren: '../pages/splash/splash.module#SplashPageModule', name: 'SplashPage', segment: 'splash', priority: 'low', defaultHistory: [] }
+                        { loadChildren: '../pages/products/products.module#ProductsPageModule', name: 'ProductsPage', segment: 'products', priority: 'low', defaultHistory: [] },
+                        { loadChildren: '../pages/splash/splash.module#SplashPageModule', name: 'SplashPage', segment: 'splash', priority: 'low', defaultHistory: [] },
+                        { loadChildren: '../pages/signup/signup.module#SignupPageModule', name: 'SignupPage', segment: 'signup', priority: 'low', defaultHistory: [] }
                     ]
                 }),
                 __WEBPACK_IMPORTED_MODULE_9__angular_fire_database__["b" /* AngularFireDatabaseModule */]
